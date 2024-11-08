425c79e309abd0714ff6318f9445ea3a8a54b8d8
abhishekhojha <abhishekhojha45@gmail.com>
2024-11-08 18:04:36 +0530

    courses model

--- models/Course.js
+++ models/Course.js
@@ -1,38 +1,139 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const courseSchema = new mongoose.Schema({
+const courseSchema = new mongoose.Schema(
+  {
     title: {
-        type: String,
-        required: true,
-        trim: true
+      type: String,
+      required: true,
+      trim: true,
     },
     description: {
-        type: String,
+      type: String,
+      required: true,
+      trim: true,
+    },
+    teachers: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
         required: true,
-        trim: true
-    },
-    teacher: {
+      },
+    ],
+    students: [
+      {
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        required: true
-    },
-    students: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User'
-    }],
-    duration: {
+        ref: "User",
+      },
+    ],
+    seo: {
+      title: {
         type: String,
-        required: true
+        trim: true,
+      },
+      description: {
+        type: String,
+        trim: true,
+      },
+      keywords: {
+        type: String,
+        trim: true,
+      },
+    },
+    categoriesIds: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Category",
+        required: true,
+      },
+    ],
+    active: Boolean,
+    duration: {
+      type: String,
+      required: true,
     },
     price: {
+      normalPrice: {
         type: Number,
-        required: true
+        required: true,
+      },
+      salePrice: {
+        type: Number,
+        required: true,
+      },
+      currency: {
+        type: String,
+        required: true,
+        default: "IN",
+      },
+      isFree: Boolean,
     },
-    category: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Category',
-        required: true
-    }
-}, { timestamps: true });
+    instructors: [
+      {
+        name: {
+          type: String,
+          required: true,
+        },
+        email: {
+          type: String,
+          required: true,
+          trim: true,
+          lowercase: true,
+          validate(value) {
+            if (!validator.isEmail(value)) {
+              throw new Error("email is invalid");
+            }
+          },
+        },
+        socialLinks: {
+          title: {
+            type: String,
+            required: true,
+            true: true,
+          },
+          url: {
+            type: String,
+            required: true,
+            true: true,
+          },
+        },
+        description: {
+          type: String,
+          trim: true,
+        },
+        imgUrl: {
+          type: String,
+          trim: true,
+        },
+      },
+    ],
+    videos: [
+      {
+        name: {
+          type: String,
+          required: true,
+        },
+        description: {
+          type: String,
+          required: true,
+        },
+        url: {
+          type: String,
+          required: true,
+        },
+      },
+    ],
+    module: {
+      total: {
+        type: Number,
+        required: true,
+      },
+      completed: {
+        type: Number,
+        required: true,
+      },
+    },
+  },
+  { timestamps: true }
+);
 
-module.exports = mongoose.model('Course', courseSchema);
+module.exports = mongoose.model("Course", courseSchema);
--- models/user.js
+++ models/user.js
@@ -5,113 +5,141 @@ const jwt = require("jsonwebtoken");
 const SecretToken = process.env.JWT_SECRET_TOKEN;
 const saltRounds = 10;
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     name: {
-        type: String,
-        required: true,
-        trim: true,
-        lowercase: true
+      type: String,
+      required: true,
+      trim: true,
+      lowercase: true,
     },
     email: {
-        type: String,
-        required: true,
-        trim: true,
-        lowercase: true,
-        validate(value) {
-            if (!validator.isEmail(value)) {
-                throw new Error("email is invalid");
-            }
+      type: String,
+      required: true,
+      trim: true,
+      lowercase: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("email is invalid");
         }
+      },
     },
     role: {
-        type: String,
-        enum: ['student', 'teacher', 'admin'],
-        default: 'student',
+      type: String,
+      enum: ["student", "teacher", "admin"],
+      default: "student",
     },
     password: {
-        type: String,
-        required: true,
-        trim: true,
-        minLength: 7,
-        validate: {
-            validator(value) {
-                return /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{7,}$/.test(value);
-            },
-            message: 'Password must be at least 7 characters long and include letters, numbers, and at least one special character'
-        }
+      type: String,
+      required: true,
+      trim: true,
+      minLength: 7,
+      validate: {
+        validator(value) {
+          return /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{7,}$/.test(
+            value
+          );
+        },
+        message:
+          "Password must be at least 7 characters long and include letters, numbers, and at least one special character",
+      },
     },
-    otp: { // Field for storing the OTP
-        type: String,
-        required: true
+    courses: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        registrationDate: {
+          type: Date,
+          required: true,
+        },
+        expirationDate: {
+          type: Date,
+          required: true,
+        },
+        ref: "Course",
+      },
+    ],
+    otp: {
+      // Field for storing the OTP
+      type: String,
+      required: true,
     },
-    otpExpiry: { // Field for storing OTP expiration time
-        type: Date,
-        required: true
+    otpExpiry: {
+      // Field for storing OTP expiration time
+      type: Date,
+      required: true,
     },
-    isVerified: { // Field to check if the user is verified
-        type: Boolean,
-        default: false
+    isVerified: {
+      // Field to check if the user is verified
+      type: Boolean,
+      default: false,
     },
-    isEmailVerified: { // Field to check if the user's email is verified
-        type: Boolean,
-        default: false
+    isEmailVerified: {
+      // Field to check if the user's email is verified
+      type: Boolean,
+      default: false,
     },
-    isBlocked: { // To block unusual acitve user
-        type: Boolean,
-        default: false
+    isBlocked: {
+      // To block unusual acitve user
+      type: Boolean,
+      default: false,
     },
-    adminApproved: { // For techers to access teachers pannel
-        type: Boolean,
-        default: false
-    }
-}, { timestamps: true });
+    adminApproved: {
+      // For techers to access teachers pannel
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
 
 // Hash the password before saving the user
 userSchema.pre("save", function (next) {
-    const user = this;
-    if (!user.isModified("password")) return next();
+  const user = this;
+  if (!user.isModified("password")) return next();
 
-    bcrypt.hash(user.password, saltRounds, function (err, hash) {
-        if (err) return next(err);
-        user.password = hash;
-        next();
-    });
+  bcrypt.hash(user.password, saltRounds, function (err, hash) {
+    if (err) return next(err);
+    user.password = hash;
+    next();
+  });
 });
 
 // Method to compare password
 userSchema.methods.comparePassword = function (password, cb) {
-    bcrypt.compare(password, this.password, function (err, isMatch) {
-        if (err) return cb(err);
-        cb(null, isMatch);
-    });
+  bcrypt.compare(password, this.password, function (err, isMatch) {
+    if (err) return cb(err);
+    cb(null, isMatch);
+  });
 };
 
 // Method to generate OTP for email verification
 userSchema.methods.generateOTP = function () {
-    const otp = crypto.randomInt(100000, 999999).toString();
-    this.otp = otp;
-    this.otpExpiry = new Date(Date.now() + 10 * 60 * 1000); // OTP expires in 10 minutes
-    return otp;
+  const otp = crypto.randomInt(100000, 999999).toString();
+  this.otp = otp;
+  this.otpExpiry = new Date(Date.now() + 10 * 60 * 1000); // OTP expires in 10 minutes
+  return otp;
 };
 
 // Method to verify OTP
 userSchema.methods.verifyOTP = function (otp) {
-    const now = new Date();
-    if (otp === this.otp && now < this.otpExpiry) {
-        this.isVerified = true;
-        this.otp = null; // Clear OTP after verification
-        this.otpExpiry = null; // Clear OTP expiry after verification
-        return true;
-    }
-    return false;
+  const now = new Date();
+  if (otp === this.otp && now < this.otpExpiry) {
+    this.isVerified = true;
+    this.otp = null; // Clear OTP after verification
+    this.otpExpiry = null; // Clear OTP expiry after verification
+    return true;
+  }
+  return false;
 };
 
 // Method to generate JWT token
 userSchema.methods.generateToken = function () {
-    if (!this.isVerified) {
-        throw new Error('User is not verified');
-    }
-    const token = jwt.sign({ id: this._id, role: this.role }, SecretToken, { expiresIn: 86400 }); // 1 day expiry
-    return token;
+  if (!this.isVerified) {
+    throw new Error("User is not verified");
+  }
+  const token = jwt.sign({ id: this._id, role: this.role }, SecretToken, {
+    expiresIn: 86400,
+  }); // 1 day expiry
+  return token;
 };
 module.exports = mongoose.model("User", userSchema);

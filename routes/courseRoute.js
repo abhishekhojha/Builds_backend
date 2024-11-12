edc42baec3b75002c0d89fdcdb8edab1be7e3f3c
abhishekhojha <abhishekhojha45@gmail.com>
2024-11-12 11:50:15 +0530

    bugs

--- routes/courseRoute.js
+++ routes/courseRoute.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const courseController = require("../controllers/courseController");
-const { courseValidationRules } = require("../middlewares/validator");
+const { courseValidationRules } = require("../middleware/validator");
 const router = express.Router();
 
 router.post("/", courseValidationRules(), courseController.createCourse);

e7fb21cf0b7a6bc38e435ff723a244650c2038bb
abhishekhojha <abhishekhojha45@gmail.com>
2024-11-12 13:28:29 +0530

    middleware

--- routes/courseRoute.js
+++ routes/courseRoute.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const courseController = require("../controllers/courseController");
 const { courseValidationRules } = require("../middleware/Validator");
-
+const { hasRole } = require('../middleware/Auth');
 const router = express.Router();
 
 router.post("/", hasRole(['admin']), courseValidationRules(), courseController.createCourse);

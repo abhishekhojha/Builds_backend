c6a5573037f3486e63c8ce2479b8f5e7d763ed0a
abhishekhojha <abhishekhojha45@gmail.com>
2024-11-20 10:39:51 +0530

    bugs

--- routes/FeedbackRoute.js
+++ routes/FeedbackRoute.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const feedbackController = require('../controllers/FeedbackController');
 const router = express.Router();
+const { hasRole } = require('../middleware/Auth');
 
 // Create a feedback form with questions
-router.post('/create', feedbackController.createFeedbackForm);
+router.post('/create',hasRole(['student']), feedbackController.createFeedbackForm);
 
 // Get a feedback form by ID
-router.get('/:id', feedbackController.getFeedbackForm);
+router.get('/:id',hasRole(['student']), feedbackController.getFeedbackForm);
 
 // Submit a feedback response
-router.post('/submit', feedbackController.submitFeedbackResponse);
+router.post('/submit',hasRole(['student']), feedbackController.submitFeedbackResponse);
 
 module.exports = router;
--- routes/OrderRoute.js
+++ routes/OrderRoute.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const { createOrder, verifyPayment } = require('../controllers/OrderController');
+const { hasRole } = require('../middleware/Auth');
 
 // Route to create order
-router.post('/create', createOrder);
+router.post('/create',hasRole(['student']), createOrder);
 
 // Route to verify payment
-router.post('/verify', verifyPayment);
+router.post('/verify',hasRole(['student']), verifyPayment);
 
 module.exports = router;

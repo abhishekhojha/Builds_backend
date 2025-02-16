9f3740f0e07736b4d4cf67a2db58aa7840fdf3a4
abhishekhojha <abhishekhojha45@gmail.com>
2025-02-16 23:19:35 +0530

    feat: add user role update and search functionality with pagination

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -20,4 +20,33 @@ router.get('/', hasRole(['admin']) ,async (req, res) => {
     res.status(500).json({ error: error.message });
   }
 });
+router.put('/:id', hasRole(['admin']), async (req, res) => {
+  
+  if(!req.params.id || !req.body.role) 
+    return res.status(400).json({ error: 'id and role is required' });
+  try {
+    const user = await User.findOneAndUpdate({ _id: req.params.id }, { role: req.body.role }, { new: true });
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+router.get("/search", hasRole(['admin']), async (req, res) => {
+  if(!req.query.q) return res.status(400).json({ error: 'q is required' });
+  console.log(req.query.q);
+  let page = 1;
+  let limit = parseInt(req.query.limit) || 20;
+  try {
+    const users = await User.find({ email: { $regex: req.query.q, $options: "i" } }).select('-password').limit(limit);
+    const totalPage = 1;
+    const data = {
+      users: users,
+      page: page,
+      totalPages: totalPage
+    }
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+})
 module.exports = router

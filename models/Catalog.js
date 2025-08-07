4ea21708082790f84b2145d7116c073988bf4fd5
yash <ymandlekar9826@gmail.com>
2025-08-07 11:56:59 +0530

    style: standardize quotation marks in Catalog model

--- models/Catalog.js
+++ models/Catalog.js
@@ -1,31 +1,35 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const catalogSchema = new mongoose.Schema(
-    {
-        title: {
-            type: String,
-            required: true,
-        },
-        description: String,
-        feedback: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'Feedback',
-            required: true,
-        },
-        videoUrl: {
-            type: String,
-            required: true,
-        },
-        thumbnail: String,
-        uploadedBy: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'User',
-            required: true,
-        },
+  {
+    title: {
+      type: String,
+      required: true,
     },
-    {
-        timestamps: true,
-    }
+    description: String,
+    feedback: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Feedback",
+      required: true,
+    },
+    videoUrl: {
+      type: String,
+      required: true,
+    },
+    thumbnail: String,
+    uploadedBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    price: {
+      type: Number,
+      default: 0,
+    },
+  },
+  {
+    timestamps: true,
+  }
 );
 
-module.exports = mongoose.model('Catalog', catalogSchema);
+module.exports = mongoose.model("Catalog", catalogSchema);
